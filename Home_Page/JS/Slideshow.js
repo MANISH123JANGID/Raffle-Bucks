b491cbfb526d59835ba7729a67daebe9a2e995c6
MANISH123JANGID <63594119+MANISH123JANGID@users.noreply.github.com>
2020-08-08 17:51:46 +0530

    COMMITTING BEFORE SOLVING THE PROBLEM OF SEARCH BAR

--- Home_Page/JS/Slideshow.js
+++ Home_Page/JS/Slideshow.js
@@ -16,7 +16,7 @@ function showSlides() {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += "active";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
